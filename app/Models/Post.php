684f5f6fcec81cbf9a33c0c64011c4c106d0676f
Alexandre Muraviov <amuraviov@globalia.ca>
2023-11-19 10:31:00 -0500

    add date casting for post published_at field

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,10 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    protected $casts = [
+        'published_at' => 'datetime:Y-m-d',
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
